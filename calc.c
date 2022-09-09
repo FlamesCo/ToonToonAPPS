301225b1a071345e1f672450702f28ba8af101a5
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-09-08 20:19:52 -0700

    Request to upload 9.8.22 1.0 v1

---
+++ calc.c
@@ -0,0 +1,9 @@
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Using ternary operator
+    (a > b) ? printf("%d is maximum", a) : printf("%d is maximum", b);
+
+    return 0;  }  ```
